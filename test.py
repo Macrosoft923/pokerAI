102be0102bfe8117011d96cbed11405af2479edb
yomi109 <168614381+yomi109@users.noreply.github.com>
2024-05-23 11:34:23 +0900

    Update test.py
    テスト用のファイル

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
 from PlayingCard import PlayingCard
-
+from judclass2 import Judge
 playingcard = PlayingCard()
+#Judge = Judge()
 dealerhands, playerhands = playingcard.getPokerHands(10)
 
 print(dealerhands)
 print(playerhands)
+
