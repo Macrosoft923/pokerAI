0044830e474595afae51743adb0a450c2266e3e7
Macrosoft923 <107292560+Macrosoft923@users.noreply.github.com>
2024-06-13 13:09:02 +0900

    ポーカーの手札生成数とプレイヤーの手札取得ロジックを修正

--- test.py
+++ test.py
@@ -1,26 +1,3 @@
-from judclass3 import Judge
-from PlayingCard import PlayingCard as pc
+from poker.hand import Range
 
-pc = pc()
-plynum = 3
-dealercards= pc.get_dealer_cards()
-playerhands = pc.get_player_hands(dealercards,plynum)
-
-# judclass2 = Judge(playerhands)
-# judclass2.judgement(playerhands)
-
-print(dealercards)
-print(playerhands)
-print(playerhands.shape)
-for i in range(plynum):
-    ju = Judge(playerhands[i])
-    print(ju.judge_hand())
-import numpy as np
-from PlayingCard import PlayingCard as pc
-
-pc = pc()
-
-dealercards = pc.get_dealer_cards()
-playerhands = pc.get_player_hands(dealercards, 10)
-
-print(np.zeros(13))
+print(Range("22+ A2+ KT+ QJ+ 32 42 52 62").percent)
